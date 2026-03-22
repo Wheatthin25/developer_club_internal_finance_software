ab07673bb2429c9285bd7a9f206b370f1ef744f6
Meaghan Baker <meaghanbaker@wireless32-214.wireless.wmich.edu>
2026-03-22 14:26:16 -0400

    with springboot

---
+++ dc_fin_soft/src/main/java/com/devwmu/dc_fin_soft/DcFinSoftApplication.java
@@ -0,0 +1,13 @@
+package com.devwmu.dc_fin_soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcFinSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcFinSoftApplication.class, args);
+	}
+
+}
---
+++ dc_fin_soft/src/test/java/com/devwmu/dc_fin_soft/DcFinSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devwmu.dc_fin_soft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcFinSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
