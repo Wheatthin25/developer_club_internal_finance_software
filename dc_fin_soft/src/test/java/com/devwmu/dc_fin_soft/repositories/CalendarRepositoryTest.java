a32bc3d50dfadf365cf1cc82c93a40d4cabec8f7
Meaghan Baker <meaghanbaker@Meaghan-1241.local>
2026-05-17 19:10:19 -0400

    fully updated testing files

--- dc_fin_soft/src/test/java/com/devwmu/dc_fin_soft/repositories/CalendarRepositoryTest.java
+++ dc_fin_soft/src/test/java/com/devwmu/dc_fin_soft/repositories/CalendarRepositoryTest.java
@@ -1,5 +1,23 @@
 package com.devwmu.dc_fin_soft.repositories;
 
-public class CalendarRepositoryTest {
-    
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.autoconfigure.*;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import com.devwmu.dc_fin_soft.repositories.CalendarRepository;
+
+
+@DataJpaTest
+class CalendarRepositoryTest {
+
+    @Autowired
+    private CalendarRepository calendarRepository;
+
+    @Test
+    void contextLoads() {
+        return;
+    }
 }
